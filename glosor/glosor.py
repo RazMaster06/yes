56d10ae6044664cb6ad377d0b83e79b9c0630881
RazMaster06 <rasmus.nordstrom777@gmail.com>
2023-10-27 14:45:40 +0200

    Lade till så att den sparar i databas

---
+++ glosor/glosor.py
@@ -0,0 +1,34 @@
+import os
+def main():
+    glosLista = {}
+
+    print("GLOSOR!")
+    while True:
+        svGlosa = input("Ange en svensk glosa: ")
+        enGlosa = input("Ange den engelska översättningen: ")
+
+        glosLista.update({svGlosa : enGlosa})
+        
+        fortsatt  = input("Vill du ange fler? j/n: ")
+        if fortsatt == "n":
+            break
+    os.system("cls")
+    
+    while True:
+       
+
+        print("\n Nu startar glosförhöret!")
+
+        for glosa in glosLista:
+            svar = input(f"\n {glosa} : ")
+
+            if svar == glosLista[glosa]:
+                print(" Rätt svar!")
+            else :
+                print(f"\nFel svar, det är {glosLista[glosa]}")
+       
+        fortsatt2  = input("Vill du köra om? j/n: ")
+        if fortsatt2 == "n":
+            break
+
+main()
